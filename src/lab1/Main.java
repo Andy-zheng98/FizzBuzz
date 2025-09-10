b58c351f31914216564847ff4eb1b53ebdac46cd
chuheng zheng <chuhengzheng@5e:3a:f2:d6:ca:05.home>
2025-09-09 21:26:44 -0400

    refactored to extract helper method

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -2,6 +2,7 @@ package lab1;
 
 public class Main {
     public static void main(String[] args) {
-        FizzBuzz.doFizzbuzz(15);}
-
+        FizzBuzz.doFizzbuzz(15);
+        FizzBuzz.main(null);
+    }
 }
